382d250058745579dd74f2dd1c1903fc4da09bde
wsynuiag <118711852+wsynuiag@users.noreply.github.com>
2024-08-06 08:52:37 +0800

    Update train_NAPGuard.py

--- train_NAPGuard.py
+++ train_NAPGuard.py
@@ -43,7 +43,7 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-import val as validate  # for end-of-epoch mAP
+import val_NAPGuard as validate  # for end-of-epoch mAP
 from models.experimental import attempt_load
 from models.yolo import Model
 from utils.autoanchor import check_anchors
